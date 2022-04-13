26d11081a5009871548f6856cd0f0f71389ca08d
Your Name <you@example.com>
2022-04-13 01:05:46 -0300

    avances js2

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -1,16 +1,30 @@
 // No cambies los nombres de las funciones.
 
+const { CONFIG_ERROR_INTERACTIVE_MODE } = require("simple-git/src/lib/tasks/clean");
+
 function obtenerMayor(x, y) {
   // "x" e "y" son números enteros (int).
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x===y){
+    return x;
+  } else if (x<y) {return y;
+  } else (x>y) ;{return x;
+  }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad >= 18)
+  { return 'Allowed';
+  } else { 
+    return "Not allowed";
+  }
+    
+  
 }
   
 function conection(status) {
